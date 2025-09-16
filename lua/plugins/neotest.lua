8ea5564ee77eca4940be40815f13be7861b0bfa3
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2025-09-16 13:35:44 -0400

    Add testing config for trouble.

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -2,49 +2,60 @@ return {
   {
     'nvim-neotest/neotest',
     lazy = true,
-    -- opts = {},
     dependencies = {
       "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
       "olimorris/neotest-rspec",
+      "ChristianChiarulli/neovim-codicons"
     },
 
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-rspec")({
-            rspec_cmd = function()
-              return vim.tbl_flatten({
-                "/Users/manlycode/.asdf/shims/bundle",
-                "exec",
-                "rspec"
+        config = function()
+          require("neotest").setup({
+            status = { virtual_text = true },
+            output = { open_on_run = true },
+            quickfix = {
+              open = function()
+                if LazyVim.has("trouble.nvim") then
+                  require("trouble").open({ mode = "quickfix", focus = false })
+                else
+                  vim.cmd("copen")
+                end
+              end,
+            },
+            adapters = {
+              require("neotest-rspec")({
+                rspec_cmd = function()
+                  return vim.tbl_flatten({
+                    "/Users/manlycode/.asdf/shims/bundle",
+                    "exec",
+                    "rspec"
+                  })
+                end
               })
-            end
+            }
           })
-        },
-      })
-    end,
-
-    keys = {
-      {"<leader>tl", "<cmd>echo 'hello line'<cr>", desc = "Run test at line", mode = "n"},
-      {
-        "<leader>ts", 
-        function() 
-          require("neotest").summary.toggle()
-        end, 
-        desc = "Toggle test summary", 
-        mode = "n"
-      },
-      {
-        "<leader>tf", 
-        function() 
-          require("neotest").run.run(vim.fn.expand("%"))
-        end, 
-        desc = "Run test at line", 
-        mode = "n"
+        end,
+        keys = {
+          {"<leader>tl", "<cmd>echo 'hello line'<cr>", desc = "Run test at line", mode = "n"},
+          {
+            "<leader>ts", 
+            function() 
+              require("neotest").summary.toggle()
+            end, 
+            desc = "Toggle test summary", 
+            mode = "n"
+          },
+          {
+            "<leader>tf", 
+            function() 
+              require("neotest").run.run(vim.fn.expand("%"))
+              require("neotest").summary.open()
+            end, 
+            desc = "Run test at line", 
+            mode = "n"
+          }
+        }
       }
     }
-  }
-}
