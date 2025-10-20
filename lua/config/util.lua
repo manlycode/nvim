a4fe6b36ddc12088543858522d1f21b3ed46d904
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2025-10-20 15:41:33 -0400

    Add helper to create language file.
    * Add

--- lua/config/util.lua
+++ lua/config/util.lua
@@ -63,6 +63,66 @@ local function split(s, delimiter)
   return result
 end
 
+vim.api.nvim_create_user_command("AddLanguage", function(cmd)
+  -- local home_path = vim.fn.expand("~")
+  local languagesPath = "~/.config/nvim/lua/language"
+
+  local languageName = cmd.args
+  local filePath = languagesPath .. "/" .. languageName .. ".lua"
+
+
+  local template = [[
+-- ------------------------------------------------------
+-- LSP
+-- ------------------------------------------------------
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+vim.lsp.config("%s",
+  {
+    capabilities = capabilities,
+    -- filetypes = { "%s" },
+    -- init_options = {
+    --  formatter = "",
+    --  linters = { "" }
+    -- },
+  }
+)
+
+vim.lsp.enable("%s")
+
+-- ------------------------------------------------------
+-- Treesitter
+-- ------------------------------------------------------
+]]
+
+  local renderedTemplate = string.format(template, languageName, languageName, languageName)
+
+  local lines = {}
+  for s in renderedTemplate:gmatch("[^\r\n]+") do
+    table.insert(lines, s)
+  end
+
+  vim.system({ "touch", filePath })
+  vim.cmd {
+    cmd = "vs",
+    args = {
+      filePath
+    }
+  }
+
+  local buffer_handle = vim.api.nvim_get_current_buf()
+  vim.api.nvim_buf_set_lines(
+    buffer_handle,
+    0,
+    0,
+    false,
+    lines
+  )
+end, {
+  desc = "Adds a plugin spec to Neovim config",
+  nargs = 1
+})
+
 vim.api.nvim_create_user_command("LazyAddPlugin", function(cmd)
   -- local home_path = vim.fn.expand("~")
   local pluginsPath = "~/.config/nvim/lua/plugins"
@@ -136,4 +196,30 @@ function RequireDir(directory_path)
       require(module_name)
     end
   end
+end
+
+local googleChrome = "\"Google Chrome\""
+local chromium = "\"Chromium\""
+local safari = "\"Safari\""
+local preferredBrowser = chromium
+local preferredBrowser = chromium
+
+function OpenWithChrome(url)
+  local command = "silent !open -a " .. preferredBrowser .. " " .. url
+  vim.cmd(command)
+end
+
+function OpenWithSafari(url)
+  local command = "silent !open " .. url
+  vim.cmd(command)
+end
+
+function OpenWithChromeDevPre()
+  local command = "silent !open -a " .. preferredBrowser .. " --args --auto-open-devtools-for-tabs "
+  vim.cmd(command)
+end
+
+function OpenWithChromeDev(url)
+  local command = "silent !open -a " .. preferredBrowser .. " --args --auto-open-devtools-for-tabs " .. url
+  vim.cmd(command)
 end
