12eb9f0f4aee5e97702a84673c3946748c5ae18b
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2025-10-24 13:44:47 -0400

    Add supertab-like completion.

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,10 +8,18 @@ return {
       'hrsh7th/cmp-path',
       -- 'hrsh7th/cmp-cmdline',
       'saadparwaiz1/cmp_luasnip',
+      "PaterJason/cmp-conjure"
     },
     -- opts = {},
+    opts = function(_, opts)
+      if type(opts.sources) == "table" then
+        vim.list_extend(opts.sources, { name = "conjure" })
+      end
+    end,
+
     config = function()
       local cmp = require 'cmp'
+      local luasnip = require("luasnip")
       -- Global setup.
       cmp.setup({
         snippet = {
@@ -36,8 +44,42 @@ return {
         mapping = cmp.mapping.preset.insert({
           ['<C-d>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          -- ['<C-Space>'] = cmp.mapping.complete(),
+          -- ['<CR>'] = cmp.mapping.confirm({ select = true }),
+
+          ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              if luasnip.expandable() then
+                luasnip.expand()
+              else
+                cmp.confirm({
+                  select = true,
+                })
+              end
+            else
+              fallback()
+            end
+          end),
+
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+              luasnip.jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
@@ -71,11 +113,6 @@ return {
 
       -- Setup lspconfig.
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-      vim.lsp.config('kotlin_lsp', {
-        config = {
-          capabilities = capabilities
-        }
-      })
 
       vim.lsp.config('lua_ls', {
         config = {
