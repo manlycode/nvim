9588d24601630390c4a84a696890e7c5c8aa9419
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2025-10-24 13:46:09 -0400

    Tweak Trouble.

--- lua/plugins/luarocks.lua
+++ lua/plugins/luarocks.lua
@@ -4,7 +4,10 @@ return {
     priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
     config = true,
     opts = {
-      rocks = { "luafilesystem" }, -- specifies a list of rocks to install
+      rocks = {
+        "luafilesystem",
+        "pimp"
+      }, -- specifies a list of rocks to install
       -- luarocks_build_args = { "--with-lua=/my/path" }, -- extra options to pass to luarocks's configuration script
     },
   }
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -4,7 +4,23 @@ return {
     dependencies = {
       'nvim-tree/nvim-web-devicons'
     },
-    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    opts = {
+      modes = {
+        preview_float = {
+          mode = "diagnostics",
+          preview = {
+            type = "float",
+            relative = "editor",
+            border = "rounded",
+            title = "Preview",
+            title_pos = "center",
+            position = { 0, -2 },
+            size = { width = 0.3, height = 0.3 },
+            zindex = 200,
+          },
+        },
+      },
+    },
     cmd = "Trouble",
     keys = {
       {
