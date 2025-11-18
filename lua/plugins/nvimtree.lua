d820891e6bfa5ea89296c8c2264fe9500e84c585
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2025-11-18 16:16:08 -0500

    Show hidden files in nvim-tree.

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -2,23 +2,25 @@ return {
   {
     "nvim-tree/nvim-tree.lua",
     opts = {
-	    sync_root_with_cwd = true,
-	    sort = {
-		    sorter = "case_sensitive",
-	    },
-	    view = {
-		    width = 30,
-	    },
-	    renderer = {
-		    group_empty = true,
-	    },
-	    filters = {
-		    dotfiles = true,
-	    },
+      sync_root_with_cwd = true,
+      sort = {
+        sorter = "case_sensitive",
+      },
+      view = {
+        width = 30,
+      },
+      renderer = {
+        hidden_display = "none",
+        group_empty = true,
+      },
+      filters = {
+        git_ignored = false,
+        dotfiles = false,
+      },
     },
     -- keys = {
-      -- { "<leader>nnn", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree Toggle" },
-      -- { "<leader>nnf", "<cmd>NvimTreeFindFile<cr>", desc = "NvimTree Find File" },
+    -- { "<leader>nnn", "<cmd>NvimTreeToggle<cr>", desc = "NvimTree Toggle" },
+    -- { "<leader>nnf", "<cmd>NvimTreeFindFile<cr>", desc = "NvimTree Find File" },
     -- },
   }
 }
