15343557a06d9e43f2b674f3f41a4c777428deb5
Chris Rittersdorf <chris.rittersdorf@collectiveidea.com>
2025-12-11 23:13:06 -0500

    Configure pylisp and basedpyright lsps.

--- lua/language/python.lua
+++ lua/language/python.lua
@@ -1,6 +1,8 @@
 -- ------------------------------------------------------
 -- LSP
 -- ------------------------------------------------------
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 vim.lsp.config("basedpyright", {
   basedpyright = {
     analysis = {
@@ -11,6 +13,21 @@ vim.lsp.config("basedpyright", {
   }
 })
 vim.lsp.enable('basedpyright')
+
+vim.lsp.config('pylsp', {
+  capabilities = capabilities,
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = { 'W391' },
+          maxLineLength = 130
+        }
+      }
+    }
+  }
+})
+vim.lsp.enable('pylsp')
 
 -- ------------------------------------------------------
 -- Treesitter
